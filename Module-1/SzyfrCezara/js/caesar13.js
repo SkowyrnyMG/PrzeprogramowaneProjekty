3bb7dcf3a5a7e688515b434d63b9dfa7699f0416
Mateusz <m.gruzla@hotmail.com>
2020-02-06 23:19:33 +0100

    Zmienione nazewnictwo zmiennych

--- Module-1/SzyfrCezara/js/caesar13.js
+++ Module-1/SzyfrCezara/js/caesar13.js
@@ -10,16 +10,17 @@ const caesar13 = string => {
 
     const stringArr = Array.from(string.toUpperCase());
     const alphabet = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-    const numbers = '1234567890';
+    const NUMBERS = '1234567890';
 
     return stringArr.reduce((newStr, cur) => {
       const curIndex = alphabet.indexOf(cur);
-      const rot13 = curIndex + 13;
-      if (numbers.includes(cur)) {
+      const JUMP_VALUE = 13;
+      const rot13 = curIndex + JUMP_VALUE;
+      if (NUMBERS.includes(cur)) {
         newStr += cur;
       } else {
         if (rot13 > 25) {
-          const encryptedIndex = rot13 % 13;
+          const encryptedIndex = rot13 % JUMP_VALUE;
           newStr += alphabet[encryptedIndex];
         } else {
           const encryptedSign = alphabet[rot13];
