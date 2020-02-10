b84319cb516d955e32060d7e869000c5a8824ef4
Mateusz <m.gruzla@hotmail.com>
2020-02-11 00:02:08 +0100

    HTML nesting and style added

--- Module-1/AST/convertASTToString.js
+++ Module-1/AST/convertASTToString.js
@@ -1,27 +1,48 @@
+let childCounter = 0;
+
 const convertASTToString = (tagName, attributes, children) => {
   let html = '';
   html += `<${tagName}`;
+
   if (attributes && attributes[0].name) {
-    attributes.forEach(attribute => {
-      html += ` ${attribute.name}="${attribute.value}"`;
-    });
+    html += attributes.reduce((attrHtml, attribute) => {
+      attrHtml += ` ${attribute.name}="${attribute.value}"`;
+      return attrHtml;
+    }, '');
     html += `>`;
   }
+
   if (children && children[0].nodeType) {
     if (children[0].nodeType === 'element') {
-      children.forEach(child => {
-        html += convertASTToString(
+      childCounter++;
+
+      html += children.reduce((childHtml, child) => {
+        childHtml += `\n${insertTabs(childCounter)}${convertASTToString(
           child.tagName,
           child.attributes,
           child.children
-        );
-      });
+        )}`;
+
+        return childHtml;
+      }, '');
+
+      childCounter--;
+      html += `\n${insertTabs(childCounter)}</${tagName}>`;
+      return html;
     }
     if (children[0].nodeType === 'text') {
       html += `${children[0].value}`;
     }
   }
   return html + `</${tagName}>`;
+};
+
+const insertTabs = counter => {
+  let tab = '';
+  for (let i = 0; i < counter; i++) {
+    tab += `\t`;
+  }
+  return tab;
 };
 
 export { convertASTToString };
--- Module-1/AST/index.js
+++ Module-1/AST/index.js
@@ -1,10 +1,21 @@
 import { convertASTToString } from './convertASTToString';
 import * as data from './ast.json';
 
-console.log(data.ast);
-
 document.querySelector('.container').innerHTML = convertASTToString(
   data.ast.tagName,
   data.ast.attributes,
   data.ast.children
 );
+
+const showConvertedHTML = () => {
+  console.log(`Converted AST from ast.json: `);
+  console.log(
+    `${convertASTToString(
+      data.ast.tagName,
+      data.ast.attributes,
+      data.ast.children
+    )}`
+  );
+};
+
+showConvertedHTML();
